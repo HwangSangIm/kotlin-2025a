212406b58aae89d7e020879c61d4d4e786156fcc
HwangSangIm <apples1243@dongyang.ac.kr>
2025-09-26 12:27:13 +0900

    week04 : Greeting App v0.2

--- AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
+++ AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
@@ -1,35 +1,21 @@
 package com.appweek04
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.content.Intent
+import com.appweek04.GreetingActivity
+import com.appweek04.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editTextName = findViewById<EditText>(R.id.editTextName)
         val buttonGreet = findViewById<Button>(R.id.buttonGreet)
-        val textViewGreeting = findViewById<TextView>(R.id.textViewGreeting)
 
         buttonGreet.setOnClickListener {
-            val name = editTextName.text.toString().trim()
-            var greeting : String = ""
-            if(name.isNotEmpty()){
-                greeting = "안녕하세요, ${name}님!"
-            }
-            else{
-                greeting = "이름을 입력해주세요."
-            }
-            textViewGreeting.text = greeting
-            textViewGreeting.visibility = View.VISIBLE
-            Log.d("KotlinWeek04App",greeting)
-
+            startActivity(Intent(this, GreetingActivity::class.java))
         }
     }
 }
